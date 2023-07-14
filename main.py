9d63bc6773c7eb7d87352f7f4e042d5be24296f8
yamakox <yamakox@gmail.com>
2023-07-14 09:27:24 +0900

    pylintによるリファクタリングを実施。

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask, make_response
+from flask import Flask
 from pyfiglet import Figlet
+
+'''banner_server main program
+
+usage:
+    python3 main.py
+'''
 
 # 参考: https://qiita.com/tommy_aka_jps/items/4cd675e3bea4c2632d4d
 figlet_standard = Figlet(font='standard')
@@ -12,15 +18,15 @@ app = Flask(__name__)
 header = {'Content-Type': 'text/plain; charset=utf-8'}
 
 @app.route('/')
-def hello():
+def _hello():
     return figlet_slant.renderText('Hello World!'), 200, header
 
 @app.route('/<text>')
-def standard(text: str):
+def _standard(text: str):
     return figlet_standard.renderText(text), 200, header
 
 @app.route('/banner/<text>')
-def banner(text: str):
+def _banner(text: str):
     return figlet_banner.renderText(text), 200, header
 
 if __name__ == '__main__':
